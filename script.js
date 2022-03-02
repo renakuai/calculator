91f8d85fb1cd3b7e88d90ff3b6e94a9b506ccefd
Rena <rena.kuai@gmail.com>
2022-03-02 10:29:30 -0600

    fixed calculation issues where it was skipping previous operator

--- script.js
+++ script.js
@@ -2,194 +2,197 @@
 const buttons = document.querySelectorAll('.button');
 buttons.forEach((element) => {
   element.addEventListener('click', () => {
-    performCalc(element);
+    createDisplayInputs(element);
    })
 })
 
-// creating display by storing values in string
-const currentTotal = document.querySelector('#currenttotal'); // select display box
-let displayValues = ''; // empty array to store typed numbers
+//creating display by storing values in string
+const displayBox = document.querySelector('#currenttotal'); // select display box
+const smallTotal = document.querySelector('#fullcalc'); // select display box
+let inputs = ''; // empty array to store all inputs
 let currentNum = ''; // empty string to store most recent num
-let total = ''; // empty string to store total;
-let operators = []; //empty array to remember operators
-let mathSymbol; // used to update display
+let total = ''; // empty to store total num;
+let operators = ''; // empty string to store operators
+let mathSymbol; // used to update operators displayed
 
-//performing relevant calc based on clicked button
-function performCalc(element) {
-    switch (element.id) {
-        case ('add') :
-            add();
-            mathSymbol = '+';
-            updateDisplayNums(element, mathSymbol);
-            break;
-        case ('sub') :
-            subtract();
-            mathSymbol = '-';
-            updateDisplayNums(element, mathSymbol);
-            break;
-        case ('mult') :
-            multiply();
-            mathSymbol = 'x';
-            updateDisplayNums(element, mathSymbol);
-            break;
-        case ('divide') :
-            divide();
-            mathSymbol = '÷';
-            updateDisplayNums(element, mathSymbol);
-            break;
-        case ('pow') :
-            power();
-            mathSymbol = '^';
-            updateDisplayNums(element, mathSymbol);
-            break;
-        case ('enter') :
-            calcTotal();
-            break;
-        case ('pos-neg') :
-            posOrNeg();
-            break;
-        case ('back') :
-            back();
-            break;
-        case ('dec') :
-            dec();
-            break;
-        case ('perc') :
-            perc();
-            break;
-        case ('clear') :
-            displayValues = ''; // empty array to store display
-            currentTotal.textContent = '';
-            currentNum = ''; // empty string to store most recent num
-            total = ''; // empty string to store total;
-            operators = [];
-            fullCalc.textContent = '';
-            break;
-        default :
-            currentNum += element.id;
-            displayValues += element.id;
-            currentTotal.textContent = currentNum;
-        }
-}
+//3+12+4
+//3+12 total = 3
+//3 total = 0
 
-//updating display
-function updateDisplayNums(element, mathSymbol) {
-    currentNum = '';
-    displayValues += mathSymbol;
-    fullCalc.textContent = displayValues; //top screen
-    currentTotal.textContent = ''; // empty text box
-    operators.push(element.id);
-}
-
- //perform final total calc and display total 
-const fullCalc = document.querySelector('#fullcalc');
-let fullNumbers = '';
-
-function calcTotal() {
-  finalOperator = operators[operators.length-1];
-  switch (finalOperator) {
+function createDisplayInputs(element) {
+  switch (element.id) {
     case 'add' : 
-     add();
+      mathSymbol = '+';
+      saveOperators(mathSymbol);
+      chooseTotal(operators);
+      createSmallDisplay()
     break;
     case 'sub' : 
-     subtract();
+      mathSymbol = '-';
+      saveOperators(mathSymbol);
+      chooseTotal(operators);
+      createSmallDisplay()
     break;
     case 'divide' : 
-     divide();
+      mathSymbol = '÷';
+      saveOperators(mathSymbol);
+      chooseTotal(operators);
+      createSmallDisplay()
     break;
     case 'mult' : 
-     multiply();
+      mathSymbol = 'x';
+      saveOperators(mathSymbol);
+      chooseTotal(operators);
+      createSmallDisplay()
     break;
     case 'pow' : 
-     power();
+      mathSymbol = '^';
+      saveOperators(mathSymbol);
+      chooseTotal(operators);
+      createSmallDisplay()
+    break;
+    case 'pos-neg' : 
+      switchPosNeg();
+    break;
+    case 'perc' : 
+      perc();
+    break;
+    case 'dec' : 
+      dec();
+    break;
+    case 'back' :
+      currentNum = currentNum.substring(0, currentNum.length - 1);
+      displayBox.textContent = currentNum;
+    break;
+    case 'clear' :
+      clear();
+      break;
+    case 'enter' : 
+      mathSymbol = '=';
+      saveOperators(mathSymbol);
+      calcFinalTotal();
+    break;
+    default:
+      currentNum += element.id; // 11
+      displayBox.textContent = currentNum; //display 12 in box
+    }
+}
+
+//saving operators display
+function saveOperators(mathSymbol) {
+  inputs += currentNum + mathSymbol; // update inputs array 11 + 12 +
+  operators += mathSymbol; // save to operators array +
+}
+
+//triggered if operator is hit
+function chooseTotal(operators) {
+  if (operators.length == 0) {
+    total = 0;
+    currentNum = '';
+    }
+  else if (operators.length == 1) {
+    total = +currentNum;
+    currentNum = '';
+    }
+  else if (operators.length > 1) {
+    calcInitialTotal();
+    }
+}
+
+function calcInitialTotal() {
+    let lastOperator = operators[operators.length-2]; // 2nd to last
+    switch (lastOperator) {
+    case '+' : 
+    add();
+    break;
+    case '-' : 
+    subtract();
+    break;
+    case '÷' : 
+    divide();
+    break;
+    case 'x' : 
+    multiply();
+    break;
+    case '^' : 
+    power();
     break;
     default: '';
     }
-    storeEquation();
-    updateResetTotals();
- }
-
- //storing equation in top part of display
- function storeEquation() {
-    currentNum = '';
-    displayValues += '=';
-    fullCalc.textContent = displayValues; //top screen
-    currentTotal.textContent = ''; // empty text box
- }
-
- //updating final total and then resetting
- function updateResetTotals() {
-    currentTotal.textContent = total;
-    currentNum = total.toString();
-    displayValues = total.toString();
-    total = '';
- }
+}
 
 // all functions
 function add() {
     total = +total + +currentNum;
+    currentNum = '';
 }
 
 function subtract() {
     total = +total - +currentNum;
+    currentNum = '';
 }
 
 function multiply() {
-  if (total == '') {
-    total = +currentNum;
-  }
-  else {
     total = +total * +currentNum; 
-    }
+    currentNum = '';
  }
 
 function divide() {
-  if (total == '') {
-    total = +currentNum;
-  }
-  else {
     total = +total / +currentNum; 
-  }
+    currentNum = '';
 }
 
 function power() {
-    if (total == '') {
-      total = +currentNum;
-    }
-    else {
-      total = Math.pow(+total , +currentNum); 
-    }
+    total = Math.pow(+total , +currentNum);
+    currentNum = '';
 }
 
-function posOrNeg() {
-    if (Math.sign(+currentNum) === -1) {
-        displayValues = displayValues.substring(1);
-     }
-     else {
-        displayValues = '-' + currentNum;
-     }
-     currentTotal.textContent = displayValues;
-     let changedNum = -1 * +currentNum;
-     currentNum = changedNum.toString();
-}
-
-function back() {
-    displayValues = displayValues.substring(0, displayValues.length - 1);
-    currentTotal.textContent = displayValues;
-    currentNum = currentNum.substring(0, currentNum.length - 1);
-}
 
 function dec() {
-    displayValues += '.' // adding . to display
     currentNum += '.'// need to make currentNum reflect decimal points
-    currentTotal.textContent = currentNum; // updating
+    displayBox.textContent = currentNum; // updating
 }
 
 function perc() {
-    displayValues += '%' // adding . to display
     currentNum += '%'// need to make currentNum reflect decimal points
-    currentTotal.textContent = currentNum; // updating
-    let toDec = +currentNum.substring(0, currentNum.length - 1) * 0.01;
-    currentNum = toDec.toString();
-
+    displayBox.textContent = currentNum; // updating
+    let convert = currentNum.substring(0, currentNum.length - 1);
+    let convertedNum = +convert * 0.01;
+    currentNum = convertedNum.toString();
 }
+
+function switchPosNeg() {
+    if (Math.sign(+currentNum) == -1) {
+        currentNum = currentNum.substring(1);
+     }
+     else {
+        currentNum = '-' + currentNum;
+     }
+     displayBox.textContent = currentNum;
+}
+
+function calcFinalTotal() {
+  calcInitialTotal(); // calc 1+2+3 = 
+  currentNum = total;
+  smallTotal.textContent = inputs; // display in small box
+  displayBox.textContent = total; // display total in big display
+  total = '';
+  inputs = '';
+  operators = '';
+}
+
+function clear() {
+  inputs = ''; // empty array to store all inputs
+  currentNum = ''; // empty string to store most recent num
+  total = ''; // empty to store total num;
+  operators = ''; // empty string to store operators
+  mathSymbol; // used to update operators displayed
+  displayBox.textContent = '';
+  smallTotal.textContent = ''; // display in small box
+}
+
+//creating small display
+function createSmallDisplay() {
+    smallTotal.textContent = inputs; // 11 + 12 + 
+    displayBox.textContent = ''; // 0 out main box
+ } 
